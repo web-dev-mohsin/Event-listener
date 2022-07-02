ec9cf06364d6327c627e5abc91e829e84e9d223b
mohsin <mdmohsineng@gmail.com>
2022-07-03 00:13:49 +0600

    learn addEvenetLitisiner with toggle

---
+++ main.js
@@ -0,0 +1,14 @@
+var headingR = document.getElementById("ha");
+
+headingR.addEventListener("click",myfunc);
+
+function myfunc(){
+    headingR.classList.toggle("style-heading");
+
+}
+
+// headingR.addEventListener("click",myfuncRs);
+
+// function myfuncRs(){
+//     headingR.classList.toggle("style-heading");
+// }
